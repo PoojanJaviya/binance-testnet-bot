11d9812752449464659cb43364645a7f69140aea
PoojanJaviya <poojanjaviya07@gmail.com>
2026-04-15 01:48:05 +0530

    refactor: refine the output

--- bot/orders.py
+++ bot/orders.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from bot.validators import validate_price, validate_quantity, validate_side
+from bot.validators import validate_price, validate_quantity, validate_side, validate_order_type
 from bot.client import BinanceFuturesClient
 
 def place_order(
@@ -7,11 +7,11 @@ def place_order(
       side : str,
       order_type : str,
       quantity : float,
-      price : Optional[float]  
+      price : Optional[float] = None 
 ): 
     side = validate_side(side=side)
     quantity = validate_quantity(quantity=quantity)
-    
+    order_type = validate_order_type(order_type=order_type)
     price = validate_price(order_type=order_type, price=price)
 
     client_wrapper = BinanceFuturesClient()
@@ -22,12 +22,15 @@ def place_order(
             response = api_client.futures_create_order(symbol=symbol, side=side, type=order_type, quantity=quantity)
         else:
             response = api_client.futures_create_order(symbol=symbol, side=side, type=order_type, quantity=quantity, price=price, timeInForce="GTC")
-
-        print(response)
+        #orderId, status, executedQty, avgPrice
+        print("Order Placed Successfully!")
+        print(f"OrderID : {response["orderId"]}")
+        print(f"Status : {response["status"]}")
+        print(f"executed Quantity : {response["executedQty"]}")
+        print(f"Average Price : {response["avgPrice"]}")
 
     except Exception as e:
         print(e)
     
     api_client.close_connection()
-
-place_order("BTCUSDT", "BUY", "LIMIT", 3, 1000)
+place_order("BTCUSDT", "BUY", "market", 0.002)
